0717a0fdb55cc867c53fec87194c16f169730571
Ziheng Wang <zihengw@stanford.edu>
2024-01-04 21:26:27 -0800

    test deploy

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 import os, sys
 from setuptools import setup, find_packages, Extension
+import distutils.sysconfig as sysconfig
 import pyarrow
+
 os.environ["CC"] = "g++"
 os.environ["CXX"] = "g++"
 
 # Define the paths for Arrow and Python
 ARROW_PATH = pyarrow.__file__.replace("/__init__.py", "")
-PYTHON_PATH = sys.executable.replace("/bin/python3", "")
+PYTHON_INCLUDE_DIR = sysconfig.get_python_inc()
+PYTHON_LIB_DIR = sysconfig.get_config_var('LIBDIR')
 PYTHON_VERSION = f"{sys.version_info.major}.{sys.version_info.minor}"
 
 # Compiler and linker flags
-extra_compile_args = ['-O3', '-g', '-fPIC', '-std=c++17', f'-I{ARROW_PATH}/include/', f'-I{PYTHON_PATH}/include/python{PYTHON_VERSION}']
-extra_link_args = ['-shared', f'-B{PYTHON_PATH}/compiler_compat', f'-L{PYTHON_PATH}/lib', '-Wl,-rpath=' + PYTHON_PATH]
+extra_compile_args = ['-O3', '-g', '-fPIC', '-std=c++17', f'-I{ARROW_PATH}/include/', f'-I{PYTHON_INCLUDE_DIR}']
+extra_link_args = ['-shared', f'-L{PYTHON_LIB_DIR}', '-Wl,-rpath=' + PYTHON_LIB_DIR]
 libraries = [':libarrow.so', ':libarrow_python.so', f':libpython{PYTHON_VERSION}.so', 'sqlite3']
 
 # Define the extension modules
@@ -21,25 +24,25 @@ extensions = [
               extra_compile_args=extra_compile_args,
               extra_link_args=extra_link_args,
               libraries=libraries,
-              library_dirs=[ARROW_PATH, PYTHON_PATH + '/lib']),
+              library_dirs=[ARROW_PATH, PYTHON_LIB_DIR + '/lib']),
     Extension('interval_nfa', 
               sources=['src/interval_nfa_cep.cpp'],
               extra_compile_args=extra_compile_args,
               extra_link_args=extra_link_args,
               libraries=libraries,
-              library_dirs=[ARROW_PATH, PYTHON_PATH + '/lib']),
+              library_dirs=[ARROW_PATH, PYTHON_LIB_DIR + '/lib']),
     Extension('interval_vector', 
               sources=['src/interval_vector_cep.cpp'],
               extra_compile_args=extra_compile_args,
               extra_link_args=extra_link_args,
               libraries=libraries,
-              library_dirs=[ARROW_PATH, PYTHON_PATH + '/lib']),
+              library_dirs=[ARROW_PATH, PYTHON_LIB_DIR + '/lib']),
     Extension('interval_dfs', 
               sources=['src/interval_dfs_cep.cpp'],
               extra_compile_args=extra_compile_args,
               extra_link_args=extra_link_args,
               libraries=libraries,
-              library_dirs=[ARROW_PATH, PYTHON_PATH + '/lib'])
+              library_dirs=[ARROW_PATH, PYTHON_LIB_DIR + '/lib'])
 ]
 setup(
     name='pyjoey',
