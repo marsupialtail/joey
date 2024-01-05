eb7afa199678e160eed4affd7f85b0e233e417a2
Ziheng Wang <zihengw@stanford.edu>
2024-01-04 23:38:19 -0800

    try again

---
+++ pre_build_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Locate the PyArrow installation directory
+PYARROW_PATH=$(python -c 'import pyarrow; print(pyarrow.__path__[0])')
+
+# Export the updated LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$PYARROW_PATH:$LD_LIBRARY_PATH"
+echo "Updated LD_LIBRARY_PATH to $LD_LIBRARY_PATH"
