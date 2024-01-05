459d7414ec037ce5ebfcbe7e1d52bebefe205ae0
Ziheng Wang <zihengw@stanford.edu>
2024-01-05 11:00:09 -0800

    test

--- set_env.sh
+++ set_env.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-pip3 install pyarrow
-# Locate the PyArrow installation directory
 PYARROW_PATH=$(python -c 'import pyarrow; print(pyarrow.__path__[0])')
-
 # Export the updated LD_LIBRARY_PATH
-export LD_LIBRARY_PATH="$PYARROW_PATH:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH=$PYARROW_PATH:$LD_LIBRARY_PATH
 echo "Updated LD_LIBRARY_PATH to $LD_LIBRARY_PATH"
